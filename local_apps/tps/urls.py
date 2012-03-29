66c3f8ec0bbf5a6591fef50202fb6b01e78332d5
Franco Coloccini <fcoloccini+github@gmail.com>
2012-03-29 09:11:42 -0300

    nuevas funcionalidades

--- local_apps/tps/urls.py
+++ local_apps/tps/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import patterns, url
+from django.conf import settings
 
 urlpatterns = patterns ('',
     url(r'^tps/$', 'local_apps.tps.views.index'),
-    url(r'^tps/(?P<legajo_id>\d+)', 'local_apps.tps.views.trabajos'),
+    url(r'^tps/(?P<legajo_id>\d+)', 'local_apps.tps.views.trabajosPracticos'),
+    url(r'^tps/error', 'local_apps.tps.views.error'),
+    url(r'^tps/nuevoTP', 'local_apps.tps.views.nuevoTP'),
 )
--- local_apps/tps/views.py
+++ local_apps/tps/views.py
@@ -1,6 +1,8 @@
 from local_apps.tps.models import Alumno
 from django.template import Context, loader
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 def index(request):
     listaAlumnos = Alumno.objects.all().order_by('-nombre')[:5]
@@ -9,3 +11,22 @@ def index(request):
                   'listaAlumnos':listaAlumnos,
     })
     return HttpResponse(t.render(c))
+
+def error(request):
+    t = loader.get_template('404.html')
+    c = Context ({
+                  'listaAlumnos':'',
+    })
+    return HttpResponse(t.render(c))
+
+def trabajosPracticos(request, legajo_id):
+    try:
+        alumno = Alumno.objects.get(nroLegajo=legajo_id)
+    except Alumno.DoesNotExist:
+        raise Http404
+    return render_to_response('tps/trabajosPracticos.html',
+                              {'alumno': alumno})
+def nuevoTP(request):
+    return render_to_response('tps/agregarTrabajosPracticos.html',
+                              context_instance=RequestContext(request))
+    
